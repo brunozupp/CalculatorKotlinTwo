68d068501f97f3b9db40ac5d116783bc8ad882c1
Bruno Noveli <brunimzuppwolf@gmail.com>
2024-01-02 22:32:10 -0300

    Layout and functionality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.novelitech.calculatorkotlintwo"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.novelitech.calculatorkotlintwo"
         minSdk = 26
@@ -59,6 +63,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/novelitech/calculatorkotlintwo/MainActivity.kt
+++ app/src/main/java/com/novelitech/calculatorkotlintwo/MainActivity.kt
@@ -1,46 +1,182 @@
 package com.novelitech.calculatorkotlintwo
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.novelitech.calculatorkotlintwo.ui.theme.CalculatorKotlinTwoTheme
+import com.novelitech.calculatorkotlintwo.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    private var canAddOperation = false
+    private var canAddDecimal = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CalculatorKotlinTwoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+//        setContentView(R.layout.activity_main)
+    }
+
+    fun numberAction(view: View) {
+        if(view is Button) {
+
+            // Melhorar aqui
+            if(view.text == ".") {
+
+                if(canAddDecimal) {
+                    binding.workingsTV.append(view.text)
+                }
+
+                canAddDecimal = false
+            } else {
+                binding.workingsTV.append(view.text)
+            }
+
+            canAddOperation = true
+        }
+    }
+
+    fun operationAction(view: View) {
+        print("olá")
+        if(view is Button && canAddOperation) {
+            binding.workingsTV.append(view.text)
+            canAddOperation = false
+            canAddDecimal = true
+        }
+    }
+
+    fun allClearAction(view: View) {
+        binding.workingsTV.text = ""
+        binding.resultsTV.text = ""
+    }
+
+    fun backSpaceAction(view: View) {
+        val length = binding.workingsTV.length()
+
+        if(length > 0) {
+            binding.workingsTV.text = binding.workingsTV.text.subSequence(0, length - 1)
+        }
+    }
+
+    fun equalsAction(view: View) {
+
+        binding.resultsTV.text = calculateResults()
+    }
+
+    private fun calculateResults(): String {
+
+        val digitsOperators = digitsOperators()
+
+        if(digitsOperators.isEmpty()) return ""
+
+        val timesDivision = timesDivisionCalculate(digitsOperators)
+
+        if(timesDivision.isEmpty()) return ""
+
+        val result = addSubtractCalculate(timesDivision)
+
+        return result.toString();
+    }
+
+    private fun addSubtractCalculate(list: MutableList<Any>): Float {
+
+        var result = list[0] as Float
+
+        for(i in list.indices) {
+            if(list[i] is Char && i != list.lastIndex) {
+
+                val operator = list[i]
+                val nextDigit = list[i + 1] as Float
+
+                if(operator == '+') {
+                    result += nextDigit
+                }
+
+//                println("AQUIIIIIIIIII ${operator == "-"}")
+
+                if(operator == '-') {
+                    result -= nextDigit
                 }
             }
         }
+
+        return result
+    }
+
+    private fun timesDivisionCalculate(digitsOperators: MutableList<Any>): MutableList<Any> {
+
+        var list = digitsOperators
+
+        while(list.contains('x') || list.contains('/')) {
+            list = calculateTimesDiv(list)
+        }
+
+        return list
+    }
+
+    private fun calculateTimesDiv(list: MutableList<Any>): MutableList<Any> {
+
+        val newList = mutableListOf<Any>()
+
+        var restartIndex = list.size
+
+        for (i in list.indices) {
+            if(list[i] is Char && i != list.lastIndex && i < restartIndex) {
+
+                val operator = list[i]
+                val prevDigit = list[i - 1] as Float
+                val nextDigit = list[i + 1] as Float
+
+                when(operator) {
+                    'x' -> {
+                        newList.add(prevDigit * nextDigit)
+                        restartIndex = i + 1
+                    }
+                    '/' -> {
+                        newList.add(prevDigit / nextDigit)
+                        restartIndex = i + 1
+                    }
+                    else -> {
+                        newList.add(prevDigit)
+                        newList.add(operator)
+                    }
+                }
+            }
+
+            if(i > restartIndex) {
+                newList.add(list[i])
+            }
+        }
+
+        return newList
+    }
+
+    private fun digitsOperators(): MutableList<Any> {
+
+        val list = mutableListOf<Any>()
+        var currentDigit = ""
+
+        for(character in binding.workingsTV.text) {
+            // isDigit = it's a number
+            if(character.isDigit() || character == '.') {
+                currentDigit += character
+            } else {
+                list.add(currentDigit.toFloat())
+                currentDigit = ""
+                list.add(character)
+            }
+        }
+
+        if(currentDigit != "") {
+            list.add(currentDigit.toFloat())
+        }
+
+        return list
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalculatorKotlinTwoTheme {
-        Greeting("Android")
-    }
-}
